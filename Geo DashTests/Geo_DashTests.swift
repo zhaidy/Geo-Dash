6071e81ec6e3b7ac3a407dc143d8e063fb79eba3
zhaidy <dongyuzhai@gmail.com>
2016-08-11 13:43:25 +0800

    Initial Commit

---
+++ Geo DashTests/Geo_DashTests.swift
@@ -0,0 +1,36 @@
+//
+//  Geo_DashTests.swift
+//  Geo DashTests
+//
+//  Created by Dongyu Zhai on 11/8/16.
+//  Copyright © 2016 Dongyu Zhai. All rights reserved.
+//
+
+import XCTest
+@testable import Geo_Dash
+
+class Geo_DashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
